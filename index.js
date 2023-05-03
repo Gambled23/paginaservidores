8046036915b47c2476d398a35603530b43a7f40d
Gambled23 <ipog71@gmail.com>
2023-05-03 12:33:41 -0600

    ptm

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-var mysql = require('mysql');
+import mysql from 'mysql';
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "usuario",
-});
+function insertarDatos() {
+    let nombreEmpleado = document.getElementById('nombreEmpleado').value
+    let edadEmpleado = document.getElementById('edadEmpleado').value
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+    
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "usuario",
+    });
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
 
-  sql = 'USE personas;'
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Result: " + result);
-  });
+        sql = 'USE personas;'
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Result: " + result);
+        });
+        sql = `INSERT INTO empleado (nombre, edad) VALUES('${nombreEmpleado}', ${edadEmpleado});`
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Result: " + result);
+        });
+    });
+}
 
-  sql = 'select * from empleado;'
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Result: " + result[0].edad);
-  });
-});
+function consultarDatos() {
+    let idEmpleado = document.getElementById('idEmpleado')
+}
